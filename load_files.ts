a4c1e6a796d4e70d38ea3ae05809c9e2db02bb65
herudi <herudi7@gmail.com>
2022-01-10 15:49:55 +0700

    sloc

--- load_files.ts
+++ load_files.ts
@@ -6,14 +6,14 @@ export const loadFiles: Handler = async (rev, next) => {
     const res = await fetch(my_fetch);
     if (!res.ok || !res.body) return next();
     const lastMod = res.headers.get("last-modified") || (await Deno.stat(new URL(my_fetch))).mtime?.getTime().toString();
-    if (res.headers.get("ETag")) {
-      response.header("ETag", res.headers.get("ETag") || "");
+    if (res.headers.get("etag")) {
+      response.header("etag", res.headers.get("etag") || "");
     } else if (lastMod) {      
       const key = btoa(lastMod);
       response.header("last-modified", lastMod);
-      response.header("ETag", `W/"${key}"`);
+      response.header("etag", `W/"${key}"`);
     }
-    if (request.headers.get("if-none-match") === response.header("ETag")) {
+    if (request.headers.get("if-none-match") === response.header("etag")) {
       return response.status(304).send();
     }
     if (request.headers.get("range")) {
