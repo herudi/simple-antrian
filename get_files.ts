cbba333e45f08ab834b4ae14353e9d18a7d34381
herudi <herudi7@gmail.com>
2022-01-11 10:30:06 +0700

    content-range

--- get_files.ts
+++ get_files.ts
@@ -11,6 +11,18 @@ const getFiles: Handler = async (rev, next) => {
     response.header("content-type", mime.getType(ext));
     if (stats.mtime) response.header("Last-Modified", stats.mtime.toUTCString());
     response.header("ETag", `W/"${stats.size}-${stats.mtime?.getTime() || rand}"`);
+    if (request.headers.get("range")) {
+      response.status(206);
+      const start = 0;
+      const end = stats.size - 1;
+      if (start >= stats.size || end >= stats.size) {
+        response.header("Content-Range", `bytes */${stats.size}`);
+        return response.send();
+      }
+      response.header("Content-Range", `bytes ${start}-${end}/${stats.size}`);
+      response.header("Content-Length", (end - start + 1).toString());
+      response.header("Accept-Ranges", "bytes");
+    }
     if (request.headers.get("if-none-match") === response.header("ETag")) {
       return response.status(304).send()
     }
